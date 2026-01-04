e78f3e99dcbefacd441712c62026597dd48c4724
Aprameya Kannan <aprameyakannan@gmail.com>
2026-01-03 22:46:37 -0500

    working quit

--- main.c
+++ main.c
@@ -1,27 +1,41 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void help() {
+	printf("\nThis is the shell. You have the following options: \nquit: exit the shell \nhelp: get information\ncd [directory]: change directory \npwd: print directory\n");
+}
+
+void quit() {
+	printf("\nExiting shell");
+	exit(0); 
+}
+
+void other_command(char input[256], char *args[256]) {
+	char* intok = strtok(input, " ");
+	int argc = 0;
+	while(intok != NULL) {
+		args[argc] = intok;
+		argc += 1;
+		intok = strtok(NULL, " ");
+	}
+	args[argc] = NULL;
+}
 
 int main() {
 	help();
 	while(1) {
-		char[256] input;
+		char input[256];
 		fgets(input, 256, stdin);
-		if(strcmp(input, "help") == 0) {
+		input[strcspn(input, "\n")] = '\0';
+		char* intok[256];
+		other_command(input, intok );
+		if(strcmp(intok[0], "help") == 0) {
 			help();
-		} else if(strcmp(input, "quit") == 0) {
+		} else if(strcmp(intok[0], "quit") == 0) {
 			quit();
 		} else {
-			
+			printf("other");
 		}	
-	}		
-
-}
-
-void help() {
-	printf("This is the shell. You have the following options:\n. quit: exit the shell\n
-		 help: get information\n cd [directory]: change directory\n pwd: print directory");
-}
-
-void quit() {
-	printf("Exiting shell");
-	exit() 
-}
+	}	
+}	
